cc04b630141e6e0e6851a8e6f4a5c88287977b53
IanToast <86629216+IanToast@users.noreply.github.com>
2022-11-28 22:15:20 -0500

    Spinning wheel prototype

--- cryptopredictor.py
+++ cryptopredictor.py
@@ -1,19 +1,49 @@
 import discord
 
-
 intents = discord.Intents.default()
-intents.members = True
+intents.message_content = True
 
-bot = discord.Bot(intents=intents)
+client = discord.Client(intents=intents)
 
-
-@bot.event
+@client.event
 async def on_ready():
-    print(f"{bot.user} ist online")
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('/random'):
+        botmess1 = await message.channel.send('poo')
+        botmess2 = await message.channel.send('poo')
+        botmess3 = await message.channel.send('poo')
+        botmess4 = await message.channel.send('poo')
+        botmess5 = await message.channel.send('poo')
+        await botmess1.edit(content="|  \               /   |")
+        await botmess2.edit(content="|      \ /\ /       |")
+        await botmess3.edit(content="|          0            |")
+        await botmess4.edit(content="|       /      \       |")
+        await botmess5.edit(content="|  /               \   |")
+
+        await botmess1.edit(content="|  \               /   |")
+        await botmess2.edit(content="|      \       /       |")
+        await botmess3.edit(content="|          0>         |")
+        await botmess4.edit(content="|       /      \       |")
+        await botmess5.edit(content="|  /               \   |")
+
+        await botmess1.edit(content="|  \               /   |")
+        await botmess2.edit(content="|      \      /        |")
+        await botmess3.edit(content="|          0            |")
+        await botmess4.edit(content="|      / \ / \      |")
+        await botmess5.edit(content="|  /               \   |")
+
+        await botmess1.edit(content="|  \               /   |")
+        await botmess2.edit(content="|      \       /       |")
+        await botmess3.edit(content="|        <0           |")
+        await botmess4.edit(content="|       /      \       |")
+        await botmess5.edit(content="|  /               \   |")
 
 
 
 
-
-
-bot.run("MTA0MjIzNjM2ODE2NTgxNDMzMg.GR8DPe.35EGYyBHOFvHsyzK6ufqbVS-xLaQpJ1sQz5G8U")
+client.run('MTA0MjIzNjM2ODE2NTgxNDMzMg.GR8DPe.35EGYyBHOFvHsyzK6ufqbVS-xLaQpJ1sQz5G8U')
